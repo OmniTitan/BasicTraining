52193370c2a794930a9166e8c5d9d9676639e024
Mark Francis <mrkfrancis8@gmail.com>
2017-10-17 10:11:02 +0100

    Added SmallestBiggest

---
+++ BasicTraining/SmallestBiggestSolution/SmallestBiggest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SmallestBiggestSolution
+{
+    public static class SmallestBiggest
+    {
+        public static int GetSmallest(int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetBiggest(int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BasicTraining/SmallestBiggestTests/SmallestBiggestTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using SmallestBiggestSolution;
+using System;
+
+namespace SmallestBiggestTests
+{
+    [TestFixture]
+    public class SmallestBiggestTest
+    {
+        [Test]
+        public void GetSmallest_FromEmptyArray_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => SmallestBiggest.GetSmallest(new int[0]));
+        }
+
+        [Test]
+        public void GetSmallest_FromPositiveArray_GetsSmallestValue()
+        {
+            Assert.AreEqual(3, SmallestBiggest.GetSmallest(new int[] { 12, 85, 6434, 3, 47 }));
+        }
+
+        [Test]
+        public void GetSmallest_FromNegativeArray_GetsSmallestValue()
+        {
+            Assert.AreEqual(-634, SmallestBiggest.GetSmallest(new int[] { -12, -85, -634, -3, -47 }));
+        }
+
+        [Test]
+        public void GetSmallest_FromMixedArray_GetsSmallestValue()
+        {
+            Assert.AreEqual(-82, SmallestBiggest.GetSmallest(new int[] { -12, -82, 636, -3, -47 }));
+        }
+
+        [Test]
+        public void GetBiggest_FromEmptyArray_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => SmallestBiggest.GetBiggest(new int[0]));
+        }
+
+        [Test]
+        public void GetBiggest_FromPositiveArray_GetsBiggestValue()
+        {
+            Assert.AreEqual(6434, SmallestBiggest.GetBiggest(new int[] { 12, 85, 6434, 3, 47 }));
+        }
+
+        [Test]
+        public void GetBiggest_FromNegativeArray_GetsBiggestValue()
+        {
+            Assert.AreEqual(-3, SmallestBiggest.GetBiggest(new int[] { -12, -85, -634, -3, -47 }));
+        }
+
+        [Test]
+        public void GetBiggest_FromMixedArray_GetsBiggestValue()
+        {
+            Assert.AreEqual(636, SmallestBiggest.GetBiggest(new int[] { -12, -82, 636, -3, -47 }));
+        }
+    }
+}
