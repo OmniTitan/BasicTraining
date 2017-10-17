d986c58cbb6e39159943b8b453b281a6be0803ba
Mark Francis <mrkfrancis8@gmail.com>
2017-10-17 10:37:23 +0100

    Added Pyramid challenge

---
+++ BasicTraining/PyramidSolution/Pyramid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PyramidSolution
+{
+    public class Pyramid
+    {
+        public const char PyramidBlock = '□';
+
+        public static string Build(int rows)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BasicTraining/PyramidTests/PyramidTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using PyramidSolution;
+using System;
+
+namespace PyramidTests
+{
+    public class PyramidTest
+    {
+        [Test]
+        public void BuildPyramid_WithNegativeRows_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Pyramid.Build(-1));
+        }
+
+        [Test]
+        public void BuildPyramid_With0Rows_ReturnsEmptyString()
+        {
+            var result = Pyramid.Build(0);
+
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [Test]
+        public void BuildPyramid_With1Row_ReturnsFlatPyramid()
+        {
+            var result = Pyramid.Build(1);
+
+            Assert.AreEqual("□", result);
+        }
+
+        [Test]
+        public void BuildPyramid_With10Rows_ReturnsPyramid()
+        {
+            var result = Pyramid.Build(10);
+
+            Assert.AreEqual(
+@"         □         
+        □□□        
+       □□□□□       
+      □□□□□□□      
+     □□□□□□□□□     
+    □□□□□□□□□□□    
+   □□□□□□□□□□□□□   
+  □□□□□□□□□□□□□□□  
+ □□□□□□□□□□□□□□□□□ 
+□□□□□□□□□□□□□□□□□□□", result);
+        }
+    }
+}
