082ca0f7c1876c0026f76f128b2aefb73cca36ae
Mark Francis <mrkfrancis8@gmail.com>
2017-10-17 11:15:08 +0100

    Created Prime Numbers project

---
+++ BasicTraining/PrimeNumbersSolution/PrimeNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumbersSolution
+{
+    public static class PrimeNumbers
+    {
+        public static IEnumerable<int> GetFactors(int factorsOf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<int> GetNPrimes(int numberOfPrimesToGet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<int> GetPrimesUpTo(int upTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsAPrimeNumber(int isThisAPrime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BasicTraining/PyramidTests/PyramidTest.cs
+++ BasicTraining/PyramidTests/PyramidTest.cs
@@ -4,6 +4,7 @@ using System;
 
 namespace PyramidTests
 {
+    [TestFixture]
     public class PyramidTest
     {
         [Test]
