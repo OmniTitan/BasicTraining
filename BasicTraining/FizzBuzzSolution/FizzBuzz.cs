8338de6178944d8f52cb894c592f8a605d690da2
Shaan Braun <miner.shane@gmail.com>
2018-07-23 07:18:03 +1000

    Completed FizzBuzz and FizzBuzz2
    Quick solution to FizzBuzz and FizzBuzz2. First attempt.

--- BasicTraining/FizzBuzzSolution/FizzBuzz.cs
+++ BasicTraining/FizzBuzzSolution/FizzBuzz.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FizzBuzzSolution
 {
@@ -7,12 +8,64 @@ namespace FizzBuzzSolution
     {
         public static IEnumerable<string> PlayFizzBuzzUpTo(int upTo)
         {
-            throw new NotImplementedException();
+            List<string> fizzBuzzResults = new List<string>();
+            for (int i = 1; i <= upTo; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    fizzBuzzResults.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    fizzBuzzResults.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    fizzBuzzResults.Add("Buzz");
+                }
+                else
+                {
+                    fizzBuzzResults.Add(i.ToString());
+                }
+            }
+
+            return fizzBuzzResults;
         }
 
         public static IEnumerable<string> PlayFizzBuzz2UpTo(int upTo, IDictionary<int, string> fizzBuzzSettings)
         {
-            throw new NotImplementedException();
+            List<string> fizzBuzzResults = new List<string>();
+            
+            if(fizzBuzzSettings == null)
+            {
+                throw new System.ArgumentNullException();
+            }
+
+            for (int i = 1; i <= upTo; i++)
+            {
+                string currentValue = "";
+
+                var sortedKeys = fizzBuzzSettings.Keys.ToList();
+                sortedKeys.Sort();
+
+                foreach(var item in sortedKeys)
+                {
+                    if(i % item == 0)
+                    {
+                        currentValue = currentValue + fizzBuzzSettings[item];
+                    }
+                }
+
+                if (currentValue == "")
+                {
+                    currentValue = i.ToString();
+                }
+
+                fizzBuzzResults.Add(currentValue);
+            }
+
+            return fizzBuzzResults;
+
         }
     }
 }
