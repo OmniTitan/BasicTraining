8dc8b6426f6a149e96fd67c140033c1a537c0845
Shaan Braun <miner.shane@gmail.com>
2018-07-26 20:36:34 +1000

    FibonacciUpTo first attempt

--- BasicTraining/FibonacciSolution/Fibonacci.cs
+++ BasicTraining/FibonacciSolution/Fibonacci.cs
@@ -7,7 +7,33 @@ namespace FibonacciSolution
     {
         public static IEnumerable<int> DoFibonacciUpTo(int numberUpTo)
         {
-            throw new NotImplementedException();
+            List<int> returnListInts = new List<int>();
+
+            if(numberUpTo <= 0)
+            {
+                return returnListInts;
+            }
+
+            int last1 = 1;
+            int last2 = 1;
+            int current;
+
+            returnListInts.Add(1);
+            returnListInts.Add(1);
+
+            while (1 > 0)
+            {
+                current = last1 + last2;
+                last2 = last1;
+                last1 = current;
+                if(current > numberUpTo)
+                {
+                    break;
+                }
+                returnListInts.Add(current);
+            }
+
+            return returnListInts;
         }
 
         public static IEnumerable<int> DoFibonacciForNValues(int n)
