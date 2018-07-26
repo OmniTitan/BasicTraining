d79df5e07b8d939d2dc6c64378961ad210b6ed59
Mark Francis <mrkfrancis8@gmail.com>
2018-07-26 14:53:17 +0100

    Realised pyramid.cs already contained the solution - have removed it

--- BasicTraining/PyramidSolution/Pyramid.cs
+++ BasicTraining/PyramidSolution/Pyramid.cs
@@ -9,17 +9,7 @@ namespace PyramidSolution
 
         public static string Build(int rows)
         {
-            List<string> pyramid = new List<string>(rows);
-
-            for (var spaceChars = rows - 1; spaceChars >= 0; --spaceChars)
-            {
-                string spacer = new string(' ', spaceChars);
-                string pyramidRow = new string(PyramidBlock, ((rows - spaceChars) * 2) - 1);
-
-                pyramid.Add(spacer + pyramidRow + spacer);
-            }
-
-            return string.Join(Environment.NewLine, pyramid);
+            throw new NotImplementedException();
         }
     }
 }
