a95841ca4452cbf2f2ef9ca7050fa0538faf0bcd
Shaan Braun <miner.shane@gmail.com>
2018-07-27 02:24:41 +1000

    completed SmallestBiggest - first attempt

--- BasicTraining/SmallestBiggestSolution/SmallestBiggest.cs
+++ BasicTraining/SmallestBiggestSolution/SmallestBiggest.cs
@@ -6,12 +6,42 @@ namespace SmallestBiggestSolution
     {
         public static int GetSmallest(int[] numbers)
         {
-            throw new NotImplementedException();
+            if(numbers.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            int lowest = numbers[0];
+
+            foreach(int i in numbers)
+            {
+                if (i < lowest)
+                {
+                    lowest = i;
+                }
+            }
+
+            return lowest;
         }
 
         public static int GetBiggest(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            int biggest = numbers[0];
+
+            foreach (int i in numbers)
+            {
+                if (i > biggest)
+                {
+                    biggest = i;
+                }
+            }
+
+            return biggest;
         }
     }
 }
